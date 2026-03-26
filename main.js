cb7113e0bb29649acbb8e04d582e9260004b04dd
melkoff <vasamelko@gmail.com>
2026-03-26 16:13:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ panels.forEach((panel, i) => {
     if (i < panels.length - 1) {
         ScrollTrigger.create({
             trigger: panel,
+            start: "top top",
             pin: true,
             pinSpacing: false
         });
@@ -23,6 +24,7 @@ panels.forEach((panel, i) => {
             opacity: 1,
             autoAlpha: 1,
             duration: 1.5,
+            ease: "power4.out",
             scrollTrigger: {
                 trigger: panel,
                 start: "top 60%", 
@@ -30,4 +32,16 @@ panels.forEach((panel, i) => {
             }
         }
     );
+
+    if (i < panels.length - 1) {
+        gsap.to(panel.querySelector('.bg-overlay'), {
+            background: "#000000CC",
+            scrollTrigger: {
+                trigger: panels[i + 1],
+                start: "top bottom",
+                end: "top top",
+                scrub: true
+            }
+        });
+    }
 });
