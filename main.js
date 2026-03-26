0ba31fd982bc8129fd3298c1a967744b0f07b497
melkoff <vasamelko@gmail.com>
2026-03-26 16:12:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ panels.forEach((panel, i) => {
     if (i < panels.length - 1) {
         ScrollTrigger.create({
             trigger: panel,
-            start: "top top",
             pin: true,
             pinSpacing: false
         });
@@ -24,7 +23,6 @@ panels.forEach((panel, i) => {
             opacity: 1,
             autoAlpha: 1,
             duration: 1.5,
-            ease: "power4.out",
             scrollTrigger: {
                 trigger: panel,
                 start: "top 60%", 
@@ -32,16 +30,4 @@ panels.forEach((panel, i) => {
             }
         }
     );
-
-    if (i < panels.length - 1) {
-        gsap.to(panel.querySelector('.bg-overlay'), {
-            background: "#000000CC",
-            scrollTrigger: {
-                trigger: panels[i + 1],
-                start: "top bottom",
-                end: "top top",
-                scrub: true
-            }
-        });
-    }
 });
